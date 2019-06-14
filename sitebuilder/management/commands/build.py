47d1fb7db2c35e6686fb905b498f85521f549c1f
xcl <chunlaixiao@163.com>
2019-06-14 21:06:27 +0800

    为上次的静态内容生成添加 一次只生成单个文件的功能，build命令可以带参数

--- sitebuilder/management/commands/build.py
+++ sitebuilder/management/commands/build.py
@@ -3,7 +3,7 @@ import shutil
 
 from django.conf import settings
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.shortcuts import reverse
 from django.test.client import Client
 
@@ -31,22 +31,39 @@ class Command(BaseCommand):
     8. 测试静态站点：cd _build, python -m http.server 9000
     """
 
+    def add_arguments(self, parser):
+        parser.add_argument('args', nargs='*')
+
     def handle(self, *args, **options):
         """request pages and build output"""
-        if os.path.exists(settings.STIE_OUTPUT_DIRECTORY):
-            shutil.rmtree(settings.STIE_OUTPUT_DIRECTORY)
-        os.mkdir(settings.STIE_OUTPUT_DIRECTORY)
+        if args:  # pages 有参数决定
+            pages = args
+            available = list(get_pages())
+            invaild = []
+            for page in pages:
+                if page not in available:
+                    invaild.append(page)
+            if invaild:
+                msg = "Invalid pass :{}".format(",".join(pages))
+                raise CommandError(msg)
+        else:  # 没有参数 pages指全部文件
+            pages = get_pages()
+            if os.path.exists(settings.STIE_OUTPUT_DIRECTORY):
+                shutil.rmtree(settings.STIE_OUTPUT_DIRECTORY)
+            os.mkdir(settings.STIE_OUTPUT_DIRECTORY)
+
         os.makedirs(settings.STATIC_ROOT, exist_ok=True)
         call_command(
             'collectstatic', interactive=False, clear=True, verbosity=0)
         client = Client()
-        for page in get_pages():
+        for page in pages:
             url = reverse('page', kwargs={'slug': page})
             response = client.get(url)
             if page == "index":
                 output_dir = settings.STIE_OUTPUT_DIRECTORY
             else:
                 output_dir = os.path.join(settings.STIE_OUTPUT_DIRECTORY, page)
-                os.makedirs(output_dir)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
             with open(os.path.join(output_dir, 'index.html'), 'wb') as f:
                 f.write(response.content)
